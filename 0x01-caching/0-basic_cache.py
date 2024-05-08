3e2dc91195d2872c0f35bc3a60abbfb1e6b30d71
Butawantemi <japhetbuta@gmail.com>
2024-05-08 22:35:42 +0300

    my last commit for today

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,25 +7,25 @@ class BasicCache(BaseCaching):
     """Implements BasicCache class"""
 
     def __init__(self):
-        """Initilizes the BasicCache class instances"""
+        """Initilizes the Basiccache class"""
         super().__init__()
 
     def put(self, key, item):
         """Adds an item to the cache"""
 
-        # Check if the key or item is None
+        # check if the key or item is None
         if key is None or item is None:
             return None
 
-        # Add item to the cache_data dictionary
+        # add item to the cache_data dictionary
         self.cache_data[key] = item
 
     def get(self, key):
         """Get an item with a specified key"""
 
-        # Check if key is None or doesn't exist in the cache
+        # check if key is None or doesn't exist in the cache
         if key is None:
             return None
 
-        # Return the item or None if key doesn't exist
-        return self.cache_data.get(key, None):
+        # return the item or None if key doesn't exist
+        return self.cache_data.get(key, None)
